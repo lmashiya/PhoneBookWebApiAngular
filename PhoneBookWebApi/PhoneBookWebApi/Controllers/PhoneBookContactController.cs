3cd5da97bcfd02674ad859f8d7794930a6d2b2db
Lehlohonolo Mashiyane <lmashiyane@iqbusiness.net>
2020-09-17 21:23:21 +0200

    Completed Migration and Added PhoneBookDetailController

---
+++ PhoneBookWebApi/PhoneBookWebApi/Controllers/PhoneBookContactController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PhoneBookWebApi.Models;
+
+namespace PhoneBookWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhoneBookContactController : ControllerBase
+    {
+        private readonly PhoneBookContext _context;
+
+        public PhoneBookContactController(PhoneBookContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PhoneBookContact
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PhoneBookContact>>> GetPhoneBookContacts()
+        {
+            return await _context.PhoneBookContacts.ToListAsync();
+        }
+
+        // GET: api/PhoneBookContact/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PhoneBookContact>> GetPhoneBookContact(int id)
+        {
+            var phoneBookContact = await _context.PhoneBookContacts.FindAsync(id);
+
+            if (phoneBookContact == null)
+            {
+                return NotFound();
+            }
+
+            return phoneBookContact;
+        }
+
+        // PUT: api/PhoneBookContact/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPhoneBookContact(int id, PhoneBookContact phoneBookContact)
+        {
+            if (id != phoneBookContact.ContactID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(phoneBookContact).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PhoneBookContactExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PhoneBookContact
+        [HttpPost]
+        public async Task<ActionResult<PhoneBookContact>> PostPhoneBookContact(PhoneBookContact phoneBookContact)
+        {
+            _context.PhoneBookContacts.Add(phoneBookContact);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPhoneBookContact", new { id = phoneBookContact.ContactID }, phoneBookContact);
+        }
+
+        // DELETE: api/PhoneBookContact/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<PhoneBookContact>> DeletePhoneBookContact(int id)
+        {
+            var phoneBookContact = await _context.PhoneBookContacts.FindAsync(id);
+            if (phoneBookContact == null)
+            {
+                return NotFound();
+            }
+
+            _context.PhoneBookContacts.Remove(phoneBookContact);
+            await _context.SaveChangesAsync();
+
+            return phoneBookContact;
+        }
+
+        private bool PhoneBookContactExists(int id)
+        {
+            return _context.PhoneBookContacts.Any(e => e.ContactID == id);
+        }
+    }
+}
---
+++ PhoneBookWebApi/PhoneBookWebApi/Migrations/20200917191820_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PhoneBookWebApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PhoneBookContacts",
+                columns: table => new
+                {
+                    ContactID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(type: "varchar(50)", nullable: false),
+                    LastName = table.Column<string>(type: "varchar(50)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "varchar(50)", nullable: false),
+                    EmailAddress = table.Column<string>(type: "varchar(50)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PhoneBookContacts", x => x.ContactID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PhoneBookContacts");
+        }
+    }
+}
