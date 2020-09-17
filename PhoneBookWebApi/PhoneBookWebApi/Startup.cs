60367f5d9fa44fd45d0a5728fedffa0342af38fa
Lehlohonolo Mashiyane <lmashiyane@iqbusiness.net>
2020-09-17 22:27:14 +0200

    Changed the launchSettings port to 5000 and Resolved camel casing for  json response

--- PhoneBookWebApi/PhoneBookWebApi/Startup.cs
+++ PhoneBookWebApi/PhoneBookWebApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
 using PhoneBookWebApi.Models;
 
 namespace PhoneBookWebApi
@@ -26,7 +27,15 @@ namespace PhoneBookWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2).AddJsonOptions(option =>
+            {
+                var contractResolver = option.SerializerSettings.ContractResolver;
+                if (contractResolver != null)
+                {
+                    (contractResolver as DefaultContractResolver).NamingStrategy = null;
+                }
+            });
+            
             services.AddDbContext<PhoneBookContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("WebApiConnection")));
         }
