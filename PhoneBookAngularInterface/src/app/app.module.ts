d11b1b9305095e839521cfd0011d2e67127ea0c0
Lehlohonolo Mashiyane <lmashiyane@iqbusiness.net>
2020-09-18 17:12:05 +0200

    added validation on inputs

--- PhoneBookAngularInterface/src/app/app.module.ts
+++ PhoneBookAngularInterface/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PhoneBookContactsComponent } from './phone-book-contacts/phone-book-contacts.component';
 import { PhoneBookContactListComponent } from './phone-book-contacts/phone-book-contact-list/phone-book-contact-list.component';
 import { PhoneBookContactComponent } from './phone-book-contacts/phone-book-contact/phone-book-contact.component';
 import {PhoneBookContactService} from './shared/phone-book-contact.service';
-import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +19,6 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     FormsModule,
-    
   ],
   providers: [PhoneBookContactService],
   bootstrap: [AppComponent]
