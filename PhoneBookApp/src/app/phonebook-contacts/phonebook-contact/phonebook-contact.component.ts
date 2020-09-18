df45b81280fc0a070f83871961c0ed72d1bca7c9
Lehlohonolo Mashiyane <lmashiyane@iqbusiness.net>
2020-09-18 20:57:44 +0200

    Added resetForm method and working on onSubmit method

--- PhoneBookApp/src/app/phonebook-contacts/phonebook-contact/phonebook-contact.component.ts
+++ PhoneBookApp/src/app/phonebook-contacts/phonebook-contact/phonebook-contact.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PhonebookContactService } from 'src/app/shared/phonebook-contact.service';
+import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-phonebook-contact',
@@ -11,6 +13,24 @@ export class PhonebookContactComponent implements OnInit {
   constructor(public service: PhonebookContactService) { }
 
   ngOnInit() {
+    this.resetForm();
+  }
+
+  onSubmit(form?:NgForm){
+    
+  }
+
+  resetForm(form?:NgForm){
+    if (form != null) {
+      form.resetForm();
+    }
+    this.service.formData = {
+      ContactID: 0,
+      FirstName: '',
+      PhoneNumber: '',
+      LastName: '',
+      EmailAddress: ''
+  }
   }
 
 }
