13543d76fb0126a292c7e2065e7abb2170901e4f
Lehlohonolo Mashiyane <lmashiyane@iqbusiness.net>
2020-09-19 13:26:41 +0200

    Addede swagger and removed the values controller

--- PhoneBookWebApi/PhoneBookWebApi/Startup.cs
+++ PhoneBookWebApi/PhoneBookWebApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json.Serialization;
 using PhoneBookWebApi.Models;
 
@@ -40,6 +41,28 @@ namespace PhoneBookWebApi
                 options.UseSqlServer(Configuration.GetConnectionString("WebApiConnection")));
 
             services.AddCors();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1.0.0.0", new OpenApiInfo
+                {
+                    Version = "v1.0.0.0",
+                    Title = "Simple PhoneBook API",
+                    Description = "CRUD operations on a contact",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Lehlohonolo Mashiyane",
+                        Email = "LehlohonoloMashiyane@gmail.com",
+                        Url = new Uri("https://github.com/lmashiya"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX...Joking use at own risk",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +76,12 @@ namespace PhoneBookWebApi
             app.UseCors(options => options.WithOrigins("http://localhost:4200/").AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin());
 
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "PhoneBook API V1");
+            });
         }
     }
 }
